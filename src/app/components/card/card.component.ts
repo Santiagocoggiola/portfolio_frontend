8a19dc3e0e4ca1e6d5b23dc1a93bf0a7c9b370cc
SantiagoCoggiola <santiago.coggiola@sumtechlabs.com>
2023-07-14 22:23:55 -0300

    Card finish

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,10 +6,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-  @Input() logoUrl: string = '';
+  @Input() logoUrl: string = 'https://upload.wikimedia.org/wikipedia/commons/5/58/UBP_V_D_1-m.png';
   @Input() title: string = 'Universidad Blas Pascal';
-  @Input() position: string = '';
+  @Input() position: string = 'Computer Engeniring';
   @Input() from: string = '2013';
   @Input() to: string = '2015';
-  @Input() content: string = 'Computer Engeniring';
+  @Input() content: string = '';
 }

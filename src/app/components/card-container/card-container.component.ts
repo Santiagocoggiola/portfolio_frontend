602462113b39a62db049d16e1fb7cce32635ef32
SantiagoCoggiola <santiago.coggiola@sumtechlabs.com>
2023-07-17 22:04:58 -0300

    experience component finish

--- src/app/components/card-container/card-container.component.ts
+++ src/app/components/card-container/card-container.component.ts
@@ -2,11 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Card } from 'src/app/domain/types';
 import { CardEditModalComponent } from '../card-edit-modal/card-edit-modal.component';
 import { Dialog } from '@angular/cdk/dialog'
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-card-container',
   templateUrl: './card-container.component.html',
-  styleUrls: ['./card-container.component.css']
+  styleUrls: ['./card-container.component.css'],
+  animations: [
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
 export class CardContainerComponent implements OnInit{
   @Input() title:string = ''; 
